6fda803d688eb310647778691358f388f44ba689
koemnak <heatkimnak@gmail.com>
2022-09-03 22:34:01 +0700

    how to create form and get data from the form to show on web pag

---
+++ Form/method.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Method GET</title>
+</head>
+<body>
+    Hi!
+    <?php
+    
+        echo $_GET["name"];
+    ?>
+    <br>
+    Your email is :
+    <?php
+        echo $_GET["email"];
+    ?>
+    
+   
+
+</body>
+</html>
